9371afc3f59c82ab2709090250e3230bbb5f204d
Cheruiyot Collins <cheruyotcheruyot@gmail.com>
2021-01-07 14:47:30 +0300

    styling almost done, font weight not working on bold and normal text as expected

--- draftjs-web/src/App.tsx
+++ draftjs-web/src/App.tsx
@@ -35,29 +35,34 @@ const editorStyleSheet = `
   @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=DM+Sans&display=swap');
   
+ 
   html *
 {
    font-size: 16px !important;
-   line-height: 24px !important;
-   color: #fff !important;
+   line-height: 28px !important;
    font-family: 'DM Sans', sans-serif !important;
-  //  background-color: #242424;
+    // font-weight: 300 ;
 }
 
   body {
     background-color: #242424;
+    color: #fff !important;
   }
-  h1 * {
-    font-size: 66px !important;
-    line-height:66px !important;
 
+  h1 * {
+    font-size: 24px !important;
+    line-height:24px !important;
+    font-weight: 700 !important;
   }
+
+  
   // html {
   //   color:white;
   //   font-family: 'DM Sans', sans-serif;
   //   font-size:16px;
   //   line-height:24px !important;
   // }
+
   // h1 > div > span > span {
   // font-size:24px !important;
   // margin-top:0px !important;
@@ -73,19 +78,20 @@ const editorStyleSheet = `
 
 
  
-  li > div > span > span {
-    // font-family: 'Chilanka', cursive;
-    // font-family: "Source Sans Pro", sans-serif;
-    font-size: 16px;
-    line-height: 18px;
-    // color: black;
-  }
-  span {
-    // font-family: "Source Sans Pro", sans-serif;
-    font-size: 15px;
-    line-height: 26px;
-    // color: black;
-  }
+  // li > div > span > span {
+  //   // font-family: 'Chilanka', cursive;
+  //   // font-family: "Source Sans Pro", sans-serif;
+  //   font-size: 16px;
+  //   line-height: 18px;
+  //   // color: black;
+  // }
+
+  // span {
+  //   // font-family: "Source Sans Pro", sans-serif;
+  //   font-size: 15px;
+  //   line-height: 26px;
+  //   // color: black;
+  // }
 `;
 
 export type defaultSourceType = 'string';
@@ -223,7 +229,7 @@ function App() {
   return (
     <>
       <style>
-        {`.public-DraftEditorPlaceholder-root{position: absolute;line-height:24px;font-size:16px;color: #969696;pointer-events: none;z-index: -10000;}${editorStyleSheet}`}
+        {`.public-DraftEditorPlaceholder-root{position: absolute;top:2px;line-height:24px;font-size:16px;color: #969696;pointer-events: none;z-index: -10000;}${editorStyleSheet}`}
       </style>
       <Editor
         ref={_draftEditorRef}
